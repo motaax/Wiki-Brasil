87c54ec2b23adb8f6c82479a67c8b65b170aa6fe
Felipe Mota <felipemota465@gmail.com>
2026-04-06 18:33:55 -0300

    Sequenciais
    Sequenciais

--- sequenciais/ex18.c
+++ sequenciais/ex18.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
 
 int main() {
-    
+    float tamanho, velocidade, tamanhoMB, tempo;
+
+    printf("Digite o tamanho do arquivo em MB: \n");
+    scanf("%f", &tamanho);
+
+    printf("Digite a velocidade da internet em Mbps: \n");
+    scanf("%f", &velocidade);
+
+    tempo = tamanhoMB / velocidade;
+    printf("O tempo aproximado e de %f segundos \n", tempo);
 
     return 0;
 }
