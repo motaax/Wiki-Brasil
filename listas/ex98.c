85a23e5dea73413955bab60cb29adc3c6cd077e4
Felipe Mota <felipemota465@gmail.com>
2026-04-11 12:20:03 -0300

    Listas
    Listas

---
+++ listas/ex98.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int inteiros[5];
+    int soma = 0;
+    int multiplicacao = 1;
+    for (int i = 0; i < 5; i++) {
+        printf("Digite o %do inteiro: ", i + 1);
+        scanf("%d", &inteiros[i]);
+        soma += inteiros[i];
+        multiplicacao *= inteiros[i];
+    }
+
+    printf("\nNumeros digitados:\n");
+
+    for (int i = 0; i < 5; i++) {
+         printf("%d ", inteiros[i]);
+        printf("\nA soma dos numeros eh %d", soma);
+        printf("\nA multiplicacao dos numeros eh %d", multiplicacao);
+    }
+       
+    return 0;
+}
